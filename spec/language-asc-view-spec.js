fb2290428b71873e8d522e0b4a712bc1c6823a06
tannervoas <39257391+tannervoas@users.noreply.github.com>
2018-05-16 14:06:54 -0700

    Initial Package

---
+++ spec/language-asc-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageAscView from '../lib/language-asc-view';
+
+describe('LanguageAscView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
